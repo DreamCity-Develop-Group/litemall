f74c1b2ea6a8e4a68810f1c656f361976184b73d
liunh1220 <547119066@qq.com>
2019-11-29 15:37:38 +0800

    modify ignore rule for node_modules

---
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/AdminApiApplication.java
@@ -0,0 +1,23 @@
+package org.linlinjava.litemall.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Wvv
+ */
+@SpringBootApplication(scanBasePackages = {"org.linlinjava.litemall.db", "org.linlinjava.litemall.core", "org" +
+        ".linlinjava.litemall.admin"})
+@MapperScan("org.linlinjava.litemall.db.dao")
+@EnableTransactionManagement
+@EnableScheduling
+public class AdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AminApiApplication.class, args);
+    }
+
+}
