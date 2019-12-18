cef2bd4f0ce67f4172e265c75d1f3ce26c544b20
liunh1220 <547119066@qq.com>
2019-12-18 11:46:36 +0800

    banner add

--- renard-wx/config/api.js
+++ renard-wx/config/api.js
@@ -2,7 +2,7 @@
 // 本机开发时使用
 var WxApiRoot = 'http://localhost:8080/wx/';
 // 局域网测试使用
-// var WxApiRoot = 'http://192.168.0.101:8080/wx/';
+var WxApiRoot = 'http://192.168.1.5:8080/wx/';
 // 云平台上线时使用
 // var WxApiRoot = 'https://litemall.menethil.com.cn/wx/';
 // var WxApiRoot = 'https://www.menethil.com.cn/wx/';
